4f85617b83e07e79a2a1087639bb788ea230471b
Jesus <jesuspl@tutanota.com>
2022-09-21 14:19:36 +0200

    Adding checkWin method

--- app/src/main/java/com/jesuspinar/random_number_guess/MainActivity.java
+++ app/src/main/java/com/jesuspinar/random_number_guess/MainActivity.java
@@ -2,7 +2,6 @@ package com.jesuspinar.random_number_guess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,10 +11,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private final static int START_ATTEMPS = 3;
     private final static int END_ATTEMPS = 0;
-    private final static int MIN_NUM = 1;
-    private final static int MAX_NUM = 10;
+    private final static int MIN_NUM = 0;
+    private final static int MAX_NUM = 9;
 
-    private int num2guess = getRandom(MIN_NUM,MAX_NUM);
     private TextView txt_numAttemps;
     private TextView txt_pickedNum;
 
@@ -30,20 +28,21 @@ public class MainActivity extends AppCompatActivity {
     private Button button8;
     private Button button9;
 
+    private int num2guess;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Init attemps
+        num2guess = getRandom(MIN_NUM,MAX_NUM);
         txt_numAttemps = findViewById(R.id.txt_numAttemps);
         txt_numAttemps.setText(START_ATTEMPS);
 
         initButtonsListeners();
     }
-
     private void initButtonsListeners() {
-        int[] numPicked = new int[1];
 
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
@@ -56,53 +55,36 @@ public class MainActivity extends AppCompatActivity {
         button8 = findViewById(R.id.button8);
         button9 = findViewById(R.id.button9);
 
-        button0.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button1.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button2.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button3.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button4.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button5.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button6.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button7.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button8.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
-        button9.setOnClickListener(view -> {
-            txt_pickedNum.setText(this.getText(0));
-            numPicked[0] = Integer.parseInt((String) txt_pickedNum.getText());
-        });
+        button0.setOnClickListener(view ->  txt_pickedNum.setText(button0.getText()));
+        button1.setOnClickListener(view ->  txt_pickedNum.setText(button1.getText()));
+        button2.setOnClickListener(view ->  txt_pickedNum.setText(button2.getText()));
+        button3.setOnClickListener(view ->  txt_pickedNum.setText(button3.getText()));
+        button4.setOnClickListener(view ->  txt_pickedNum.setText(button4.getText()));
+        button5.setOnClickListener(view ->  txt_pickedNum.setText(button5.getText()));
+        button6.setOnClickListener(view ->  txt_pickedNum.setText(button6.getText()));
+        button7.setOnClickListener(view ->  txt_pickedNum.setText(button7.getText()));
+        button8.setOnClickListener(view ->  txt_pickedNum.setText(button8.getText()));
+        button9.setOnClickListener(view ->  txt_pickedNum.setText(button9.getText()));
+
 
         ///////////////////////
-        checkWin(numPicked[0]);
+        checkWin(Integer.parseInt((String) txt_pickedNum.getText()));
     }
 
     private void checkWin(int numPicked) {
+        int numAttemps = Integer.parseInt((String) txt_numAttemps.getText());
 
+        if(numPicked == num2guess && numAttemps >= END_ATTEMPS ){
+            Toast.makeText(this,"You win !!", Toast.LENGTH_LONG).show();
+            onDestroy();
+        }
+        else if(numAttemps <= END_ATTEMPS){
+            Toast.makeText(this,"You lose !!", Toast.LENGTH_LONG).show();
+            onDestroy();
+        }
+        else{
+            txt_numAttemps.setText(Integer.parseInt((String) txt_pickedNum.getText()) - 1);
+        }
     }
 
     public int getRandom(int min, int max){
